53f50b60f3a96e20254934162e390ac0113ce68e
Duncan-lin <duncan@alion.jp>
2024-07-16 18:54:15 +0800

    enable of cors of all origin

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
-import http from 'http';
-import { Server } from 'socket.io';
-import fs from 'fs';
+import http from "http";
+import { Server } from "socket.io";
+import fs from "fs";
 
 const server = http.createServer((req, res) => {
-	fs.readFile('index.html', (err, data) => {
-		if (err) {
-			res.writeHead(500);
-			return res.end('Error loading index.html');
-		}
-		res.writeHead(200);
-		res.end(data);
-	});
+  fs.readFile("index.html", (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      return res.end("Error loading index.html");
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
 });
 
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*", // Allow all origins
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+    credentials: true,
+  },
+});
 
-io.on('connection', (socket) => {
-	console.log('A user connected');
+io.of("/swise-channel01").on("connection", (socket) => {
+  console.log("A user connected");
+  // socket.join("swise01");
 
-	socket.on('disconnect', () => {
-		console.log('User disconnected');
-	});
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
 
-	socket.on('swise-test', (msg) => {
-		console.log('message: ' + msg);
-		io.emit('swise-test', msg);
-	});
+  socket.on("swise-test", (msg) => {
+    console.log("message: " + msg);
+    io.of("/swise-channel01").emit("swise-test", msg);
+  });
 });
 
 const PORT = process.env.PORT || 25565;
 
 server.listen(PORT, () => {
-	console.log(`Server running on http://127.0.0.1:${PORT}`);
+  console.log(`Server running on http://127.0.0.1:${PORT}`);
 });
